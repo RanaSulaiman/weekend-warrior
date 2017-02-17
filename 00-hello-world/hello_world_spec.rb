cf364814c020595bbbaafd89a1c7ca4b45b11e21
Rana Sulaiman <rs_sulaiman@hotmail.com>
2017-02-17 11:15:33 -0800

    Deleting skip

--- 00-hello-world/hello_world_spec.rb
+++ 00-hello-world/hello_world_spec.rb
@@ -9,14 +9,17 @@ describe "Hello World" do
   end
 
   it "When given 'Alice' it should greet Alice!" do
+
     expect(hello_world 'Alice').must_equal 'Hello, Alice!'
   end
 
   it "When given 'Bob' it should greet Bob!" do
+
     expect(hello_world 'Bob').must_equal 'Hello, Bob!'
   end
 
   it "When given an empty string it should greet the world!" do
+    
     expect(hello_world '').must_equal 'Hello, World!'
   end
 end
