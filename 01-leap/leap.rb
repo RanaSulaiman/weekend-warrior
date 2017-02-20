0b22fc087c6bbd97b670260e09f7e62d370d8c48
Rana Sulaiman <rs_sulaiman@hotmail.com>
2017-02-19 21:12:23 -0800

    commited the lines that call the method

--- 01-leap/leap.rb
+++ 01-leap/leap.rb
@@ -4,10 +4,10 @@ def leap_year?(year)
   year % 4 == 0 && !(year % 100 == 0) || year % 400 == 0
 end
 
-leap_year?(1996)
-leap_year?(1997)
-leap_year?(1998)
-leap_year?(1900)
-leap_year?(2400)
-leap_year?(2000)
-leap_year?(8)
+# leap_year?(1996)
+# leap_year?(1997)
+# leap_year?(1998)
+# leap_year?(1900)
+# leap_year?(2400)
+# leap_year?(2000)
+# leap_year?(8)
